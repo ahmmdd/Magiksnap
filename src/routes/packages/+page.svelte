2171d206925e0defb8d6a110866da218ef58c904
ahmmdd <ahmedmohammedjuned@gmail.com>
2024-04-10 03:21:50 -0400

    added desc

--- src/routes/packages/+page.svelte
+++ src/routes/packages/+page.svelte
@@ -4,3 +4,10 @@
 <br>
 <br>
 <h1>Coming Soon!</h1>
+<p>At MagikSnap Corp., we're dedicated to making your event unforgettable with our exceptional photobooth packages. Whether you're planning a wedding, birthday bash, corporate event, or any special occasion, we have the perfect package to suit your needs.
+
+    Our packages are designed to provide everything you need for a seamless and entertaining photobooth experience. From high-quality props to customizable backdrops, we ensure that every detail is taken care of, allowing you and your guests to focus on creating lasting memories.
+    
+    Choose from a variety of packages tailored to fit different budgets and preferences. Our basic packages include essential features such as unlimited photo prints, digital copies, and a friendly onsite attendant to assist your guests throughout the event. For those looking for an extra touch of luxury, our premium packages offer additional enhancements like themed props, custom backdrop options, and even social media integration for instant sharing.
+    
+    No matter which package you choose, you can count on MagikSnap Corp. to deliver top-notch service and unparalleled entertainment value. Let us bring the fun to your next event and turn moments into memories that will be cherished for years to come. Contact us today to book your photobooth package and let the magic begin!</p>
